45bd3805c82efc4b7c321f304e469213085cd6f9
Maximilian Fricke <mfricke2808@gmail.com>
2021-12-12 09:48:28 +0100

    day 12

---
+++ day12.lua
@@ -0,0 +1,48 @@
+local iter = require "lib/iter"
+
+local graph = { nodes = {} }
+for line in io.input("input/day12"):lines() do
+    local edge = iter:new(line:gmatch("%a+")):collect()
+
+    for node in iter:new(edge) do
+        if not graph.nodes[node] then
+            local small = node == node:lower()
+            graph.nodes[node] = { neighbors = {}, small = small }
+        end
+    end
+
+    table.insert(graph.nodes[edge[1]].neighbors, edge[2])
+    table.insert(graph.nodes[edge[2]].neighbors, edge[1])
+end
+
+local function contains (list, item)
+    for _, v in ipairs(list) do
+        if v == item then return true end
+    end
+    return false
+end
+
+local function dfs (path, revisit)
+    if path[#path] == "end" then return iter:new({ path }) end
+
+    return coroutine.wrap(function ()
+        for _, neighbor in ipairs(graph.nodes[path[#path]].neighbors) do
+            if neighbor ~= "start" then
+                local visit, revisit = true, revisit
+                if graph.nodes[neighbor].small then
+                    visit = revisit or not contains(path, neighbor)
+                    revisit = revisit and not contains(path, neighbor)
+                end
+
+                if visit then
+                    table.insert(path, neighbor)
+                    for p in dfs(path, revisit) do coroutine.yield(p) end
+                    table.remove(path, #path)
+                end
+            end
+        end
+    end)
+end
+
+print("Part 1:", iter:new(dfs({ "start" }, false)):count())
+print("Part 2:", iter:new(dfs({ "start" }, true)):count())
